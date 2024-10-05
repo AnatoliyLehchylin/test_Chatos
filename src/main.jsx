8e593e5aa1952cbcb3c82e5606b87b43f1eae55e
sigibambarbiev <sigibambarbiev@gmail.com>
2024-10-05 12:43:11 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import {BrowserRouter as Router} from 'react-router-dom';
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-    <Router>
         <App/>
-    </Router>
 )
